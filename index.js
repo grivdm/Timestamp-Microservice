546a01263b8eef7d9bd864f151ff2b1c2fe8f3c5
grivdm <grivdm94@gmail.com>
2023-02-05 22:21:46 -0500

    First commit

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
+//index
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/:data?", (req, res) => {
+  const reqData = req.params.data;
+  let date;
+
+  if (!isNaN(reqData)) {
+    date = new Date(parseInt(reqData));
+  } else {
+    date = new Date(reqData);
+  }
+
+  if (isNaN(date.getTime())) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const port = 3000;
+var listener = app.listen(port, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
